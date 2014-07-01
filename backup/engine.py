4aede0a5a0a3f19c69de3a790087e718d4e61151
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-07-01 17:45:40 -0400

    Coded status property of Snapshot and removed "name" parameter.

--- backup/engine.py
+++ backup/engine.py
@@ -43,6 +43,24 @@ import time
 import threading
 
 
+# Status constants for Snapshot objects.
+VOID = 0
+BLANK = 1
+SYNCING = 2
+COMPLETE = 3
+DELETING = 4
+DELETED = 5
+# Status lookup for logging purposes.
+_status_lookup = {
+    0: "VOID",
+    1: "BLANK",
+    2: "SYNCING",
+    3: "COMPLETE",
+    4: "DELETING",
+    5: "DELETED",
+    }
+
+
 class rsyncWrapper:
 
     """Manages an rsync subprocess and threads that log its output streams."""
@@ -136,55 +154,79 @@ class Snapshot:
 
     """Abstraction object for a backup snapshot.
 
-    dest -- Path to the root of the backup directory.
-    name -- Name of the backup (usually a host name).
-    interval -- Name of the backup cycle (i. e. hourly, daily, etc.)
-    index -- Index number of the individual snapshot in the cycle.
+    Parameters:
+        dir -- Path to the directory where snapshots are located.
+        interval -- Name of the backup cycle (i. e. hourly, daily, etc.)
+        index -- Index number of the individual snapshot in the cycle.
 
     The constructor's parameters are components to build the snapshot's path:
-        /dest/name/interval.timestamp
+        /dir/interval.timestamp
     If index is None, the timestamp will be the current date and time.
-    If index is a positive integer, existing snapshot directories of the same
+    If index is an integer, existing snapshot directories of the same
     interval will be sorted and the one at the given index number will
     be represented.
+
+    Properties:
+        timestamp
+        stimestamp -- ISO 8601 string of the timestamp
+        status
+        path
+        lockfile
+        statusfile
+
+    Attributes:
+        dir
+        interval
+        index
+
+    Methods:
+        delete
+        mkdir
     """
 
     _timeformat = "%Y-%m-%dT%H:%M"  # ISO 8601 format: yyyy-mm-ddThh:mm
 
-    def __init__(self, dest, name, interval, index=None):
+    def __init__(self, dir, interval, index=None):
         self._logger = logging.getLogger(__name__+"."+self.__class__.__name__)
         self._timestamp = None
-        self.name = name
+        self._status = VOID
         self.interval = interval
         self.index = index
-        self.dest = dest
+        self.dir = dir
         self.timestamp  # Raises IndexError if self.index is out of range.
 
     @property
     def path(self):
         path = os.path.join(
-            self.dest,
-            self.name,
+            self.dir,
             self.interval+"."+self.stimestamp
             )
         return path
 
     @property
     def lockfile(self):
-        return self.path + ".lock"
+        path = os.path.join(
+            self.dir,
+            "."+self.interval+"."+self.stimestamp+".lock"
+            )
+        return path
 
-    def get_status(self):
-        # Status: blank, locked, dirty, complete, deleting, syncing
-        pass
+    @property
+    def statusfile(self):
+        path = os.path.join(
+            self.dir,
+            "."+self.interval+"."+self.stimestamp+".status"
+            )
+        return path
 
     def mkdir(self):
         """Create the snapshot directory on the filesystem."""
-        try:
-            os.mkdir(self.path)
-        except OSError:
-            err = sys.exc_info()[1]
-            if err.errno != errno.EEXIST:
-                raise
+        if self.status != VOID:
+            msg = "status is {}, must be VOID.".format(self.status)
+            raise RuntimeError(msg)
+        os.mkdir(self.path)
+        self.status = BLANK  # VOID -> BLANK
+        self._logger.debug("Created directory {}.".format(self.path))
 
     @property
     def timestamp(self):
@@ -192,12 +234,12 @@ class Snapshot:
         if self._timestamp is None:
             if self.index is None:
                 self._timestamp = datetime.datetime.now()
+                self.status = VOID
             else:
                 # Try to find timestamp by index in existing directories.
                 dirs = glob.glob(
-                    "{}/{}/{}.????-??-??T??:??".format(
-                        self.dest,
-                        self.name,
+                    "{}/{}.????-??-??T??:??".format(
+                        self.dir,
                         self.interval
                         )
                     )
@@ -206,23 +248,70 @@ class Snapshot:
                     dirs[self.index].rsplit(".")[-1],  # raises IndexError.
                     self._timeformat
                     )
+                self._status = COMPLETE
         return self._timestamp
 
     @timestamp.setter
     def timestamp(self, value):
         assert isinstance(value, datetime.datetime), type(value)
         self._timestamp = value
+        self._logger.debug("timestamp set to {}.".format(self.stimestamp))
 
     @property
     def stimestamp(self):
         """The timestamp as a ISO 8601 string."""
         return self.timestamp.strftime(self._timeformat)
 
-    def delete(self):
-        pass
+    @property
+    def status(self):
+        """Status of this Snapshot.
 
-    def acquire(self, timeout=None):
-        # Inspired by https://github.com/smontanaro/pylockfile
+        VOID -> BLANK -> SYNCING -> COMPLETE -> DELETING -> DELETED
+
+        VOID : Snapshot doesn't exist on the filesystem.
+        BLANK : Directory exists and is empty.
+        SYNCING : Synchronization is in progress.
+        COMPLETE : Is safe to link-dest against.
+        DELETING : In the process of being removed from the filesystem.
+        DELETED : Snapshot is deleted and can no longer change status.
+        """
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        assert value in (VOID, BLANK, SYNCING, COMPLETE, DELETING, DELETED)
+        self._logger.debug(
+            "Changing status from {} to {}.".format(
+                _status_lookup[self._status],
+                _status_lookup[value]
+                )
+            )
+        if self.status == DELETED and value != self.status:
+            msg = "Cannot change the status of a deleted snapshot."
+            raise RuntimeError(msg)
+        if value in (SYNCING, DELETING):
+            if not self.is_locked():
+                msg = "Snapshot must be locked to enter this status."
+                raise RuntimeError(msg)
+            with open(self.statusfile, "w") as f:
+                f.write(str(value))
+        else:
+            try:
+                os.unlink(self.statusfile)
+            except FileNotFoundError:
+                pass
+        self._status = value
+
+    def delete(self):
+        self._logger.info("Deleting {}.".format(self.path))
+        # Mark snapshot as dirty, status as deleting, or something.
+        with self:
+            self.status = DELETING
+            shutil.rmtree(self.path)
+            self.status = VOID
+        self._logger.info("Deletion complete.")
+
+    def acquire(self):
         try:
             os.mkdir(self.lockfile)  # Atomic operation.
         except OSError:
@@ -236,12 +325,24 @@ class Snapshot:
     def release(self):
         os.rmdir(self.lockfile)
 
+    def is_locked(self):
+        try:
+            self.acquire()
+            self.release()
+            return False
+        except RuntimeError:  #XXX Will be changed for a different exception.
+            return True
+
     def __enter__(self):
         self.acquire()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.release()
+        if exc_type is not None:
+            # Leave the lock in place if an exception was raised.
+            return False  # Exception will be re-raised in the calling context.
+        else:
+            self.release()
 
     @staticmethod
     def from_path(path):
